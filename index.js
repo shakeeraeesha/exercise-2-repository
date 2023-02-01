8705229662d3c7e51620bacc4bc583165cdc75e3
shakeeraeesha <shakeeraeesha@gmail.com>
2023-02-01 11:59:16 -0500

    Created quote app

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 function printQuote() {
     
+    var string1 = "lorem ipsum 1"
+    var string2 = "lorem ipsum 2"
+    var string3 = "lorem ipsum 3"
     quotation = Math.floor(Math.random() * 3)
     
     if (quotation == 1) {
-        print("lorem ipsum 1")
+        print(string1)
     }
 
     if (quotation == 2) {
-        print("lorem ipsum 2")
+        print(string2)
     }
 
     else {
-        print("lorem ipsum 3")
+        print(string3)
     }
     
 }
